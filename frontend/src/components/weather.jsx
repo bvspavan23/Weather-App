f2ccf88a6c0346ebef721b7c55f079b8b37363e2
B.V.S.Pavan <bvspavan234@gmail.com>
2025-05-10 23:53:49 +0530

    CHANGED UI

--- frontend/src/components/weather.jsx
+++ frontend/src/components/weather.jsx
@@ -9,21 +9,39 @@ import { Gauge } from "lucide-react";
 import Clouds from "../assets/clouds.mp4";
 import Card from "./card";
 import axios from "axios";
-
 const WeatherApp = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
   const [loading, setLoading] = useState(false);
   const [showForecast, setShowForecast] = useState(false);
-  const URL="https://weather-app-z3l8.onrender.com";
+  const [Name, setName] = useState(null);
+  const GEO_CODE = "http://api.openweathermap.org/geo/1.0/direct?";
+  const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
+  const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?";
+  const KEY =import.meta.env.VITE_API_KEY;
+  
+  const coordinates = async (q) => {
+    try {
+      const url = `${GEO_CODE}q=${q}&appid=${KEY}`;
+      const response = await axios.get(url);
+      const name = response.data[0].name;
+      const lat = response.data[0].lat;
+      const lon = response.data[0].lon;
+      const res = { lat, lon, name };
+      return res;
+    } catch (err) {
+      console.log("ERROR:", err);
+    }
+  };
+
   const fetchWeather = async () => {
     if (!city) return;
     setLoading(true);
     try {
-      const response = await axios.post(`${URL}/api/v1/weather`, {
-        q: city,
-      });
+      const { lat, lon,name} = await coordinates(city);
+      setName(name);
+      const response = await axios.get(`${BASE_URL}lat=${lat}&lon=${lon}&appid=${KEY}`);
       setWeather(response.data);
       setShowForecast(false);
     } catch (error) {
@@ -31,14 +49,12 @@ const WeatherApp = () => {
     }
     setLoading(false);
   };
-
   const fetchForecast = async () => {
     if (!city) return;
     setLoading(true);
     try {
-      const response = await axios.post(`${URL}/api/v1/weather/forecast`, {
-        q: city,
-      });
+      const { lat, lon,name} = await coordinates(city);
+      const response = await axios.get(`${FORECAST_URL}lat=${lat}&lon=${lon}&cnt=${1}&appid=${KEY}`);
       setForecast(response.data);
       setShowForecast(true);
     } catch (error) {
@@ -51,7 +67,6 @@ const WeatherApp = () => {
 
   return (
     <div className="relative min-h-screen overflow-hidden">
-
       <video
         autoPlay
         muted
@@ -89,9 +104,9 @@ const WeatherApp = () => {
         </div>
 
         {loading && (
-        <div className="mb-6 flex items-center gap-3 text-white text-lg">
-          <div className="w-6 h-6 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
-              Fetching weather details...
+          <div className="mb-6 flex items-center gap-3 text-white text-lg">
+            <div className="w-6 h-6 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+            Fetching weather details...
           </div>
         )}
 
@@ -110,14 +125,46 @@ const WeatherApp = () => {
               transition={{ duration: 0.5 }}
               className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-4xl"
             >
-              <Card title="Location" content={`${weather.name}, ${weather.data.sys.country}`} icon={<CiLocationOn />} />
-              <Card title="Status" content={weather.data.weather[0].description} icon={<TiWeatherPartlySunny />} />
-              <Card title="Coordinates" content={`Lat: ${weather.data.coord.lat}, Lon: ${weather.data.coord.lon}`} icon={<FaLocationCrosshairs />} />
-              <Card title="Temperature" content={`${kelvinToCelsius(weather.data.main.temp)}°C`} icon={<FaTemperatureHigh />} />
-              <Card title="Feels Like" content={`${kelvinToCelsius(weather.data.main.feels_like)}°C`} icon={<FaTemperatureHigh />} />
-              <Card title="Humidity" content={`${weather.data.main.humidity}%`} icon={<WiCloudyWindy />} />
-              <Card title="Wind Speed" content={`${(weather.data.wind.speed * 3.6).toFixed(2)} km/h`} icon={<WiCloudyWindy />} />
-              <Card title="Pressure" content={`${weather.data.main.pressure} hPa`} icon={<Gauge />} />
+              <Card
+                title="Location"
+                content={`${Name}, ${weather.sys.country}`}
+                icon={<CiLocationOn />}
+              />
+              <Card
+                title="Status"
+                content={weather.weather[0].description}
+                icon={<TiWeatherPartlySunny />}
+              />
+              <Card
+                title="Coordinates"
+                content={`Lat: ${weather.coord.lat}, Lon: ${weather.coord.lon}`}
+                icon={<FaLocationCrosshairs />}
+              />
+              <Card
+                title="Temperature"
+                content={`${kelvinToCelsius(weather.main.temp)}°C`}
+                icon={<FaTemperatureHigh />}
+              />
+              <Card
+                title="Feels Like"
+                content={`${kelvinToCelsius(weather.main.feels_like)}°C`}
+                icon={<FaTemperatureHigh />}
+              />
+              <Card
+                title="Humidity"
+                content={`${weather.main.humidity}%`}
+                icon={<WiCloudyWindy />}
+              />
+              <Card
+                title="Wind Speed"
+                content={`${(weather.wind.speed * 3.6).toFixed(2)} km/h`}
+                icon={<WiCloudyWindy />}
+              />
+              <Card
+                title="Pressure"
+                content={`${weather.main.pressure} hPa`}
+                icon={<Gauge />}
+              />
             </motion.div>
           </>
         )}
@@ -137,14 +184,54 @@ const WeatherApp = () => {
               transition={{ duration: 0.5 }}
               className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-4xl"
             >
-              <Card title="Location" content={`${forecast.name}`} icon={<CiLocationOn />} />
-              <Card title="Weather" content={forecast.data.list[0].weather[0].description} icon={<TiWeatherPartlySunny />} />
-              <Card title="Min Temp" content={`${kelvinToCelsius(forecast.data.list[0].main.temp_min)}°C`} icon={<FaTemperatureHigh />} />
-              <Card title="Max Temp" content={`${kelvinToCelsius(forecast.data.list[0].main.temp_max)}°C`} icon={<FaTemperatureHigh />} />
-              <Card title="Feels Like" content={`${kelvinToCelsius(forecast.data.list[0].main.feels_like)}°C`} icon={<FaTemperatureHigh />} />
-              <Card title="Humidity" content={`${forecast.data.list[0].main.humidity}%`} icon={<WiCloudyWindy />} />
-              <Card title="Pressure" content={`${forecast.data.list[0].main.pressure} hPa`} icon={<Gauge />} />
-              <Card title="Wind Speed" content={`${(forecast.data.list[0].wind.speed * 3.6).toFixed(2)} km/h`} icon={<WiCloudyWindy />} />
+              <Card
+                title="Location"
+                content={`${Name}`}
+                icon={<CiLocationOn />}
+              />
+              <Card
+                title="Weather"
+                content={forecast.list[0].weather[0].description}
+                icon={<TiWeatherPartlySunny />}
+              />
+              <Card
+                title="Min Temp"
+                content={`${kelvinToCelsius(
+                  forecast.list[0].main.temp_min
+                )}°C`}
+                icon={<FaTemperatureHigh />}
+              />
+              <Card
+                title="Max Temp"
+                content={`${kelvinToCelsius(
+                  forecast.list[0].main.temp_max
+                )}°C`}
+                icon={<FaTemperatureHigh />}
+              />
+              <Card
+                title="Feels Like"
+                content={`${kelvinToCelsius(
+                  forecast.list[0].main.feels_like
+                )}°C`}
+                icon={<FaTemperatureHigh />}
+              />
+              <Card
+                title="Humidity"
+                content={`${forecast.list[0].main.humidity}%`}
+                icon={<WiCloudyWindy />}
+              />
+              <Card
+                title="Pressure"
+                content={`${forecast.list[0].main.pressure} hPa`}
+                icon={<Gauge />}
+              />
+              <Card
+                title="Wind Speed"
+                content={`${(forecast.list[0].wind.speed * 3.6).toFixed(
+                  2
+                )} km/h`}
+                icon={<WiCloudyWindy />}
+              />
             </motion.div>
           </>
         )}
